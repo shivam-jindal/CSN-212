90474e15488cd970875b343fd593a54d4d6e936c
shivam <shivamjindal39@gmail.com>
2017-02-27 21:56:59 +0530

    initialize second problem, arrange in folder

---
+++ Assignment-2(DP)/problem1.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestZigZag(int inputSequence[], int n)
+{
+
+    int dp1[n], dp2[n];
+    /*dp1[i] = Length of the longest Zig-Zag subsequence ending at index i and
+      dp2[i]= Length of the longest Zig-Zag subsequence ending at index i and dp[i]<dp[i-1]
+    */
+
+    for (int i = 0; i < n; i++)
+        dp1[i] = dp2[i] = 1;
+
+    int answer = 1;
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            // If inputSequence[i] is greater, then check with dp2[j]
+            if (inputSequence[j] < inputSequence[i] && dp1[i] < dp2[j] + 1)
+                dp1[i] = dp2[j] + 1;
+
+            // If inputSequence[i] is smaller, then check with dp1[j]
+            if( inputSequence[j] > inputSequence[i] && dp2[i] < dp1[j] + 1)
+                dp2[i] = dp1[j] + 1;
+        }
+
+        //maximum of both values at index i will be the answer
+        if (answer < max(dp1[i], dp2[i]))
+            answer = max(dp1[i], dp2[i]);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    int testInput[] = { 1, 7, 4, 9, 2, 5 };
+    int n = sizeof(testInput)/sizeof(testInput[0]);
+    cout<<"Length of Longest Zig-Zag subsequence of test input is "<<longestZigZag(testInput, n)<<endl;
+
+    return 0;
+}
---
+++ Assignment-2(DP)/problem2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long numWays(int width, int height, string[] bad){
+    long answer = 0;
+
+
+    return answer;
+}
+
+int main(){
+    //check for a test input
+    int width = 6;
+    int height = 6;
+    string[] bad = {"0 0 0 1","6 6 5 6"};
+
+    cout<<"Number of distinct paths for test input is "<<numWays(width, height, bad)<<endl;
+
+    return 0;
+}
+
