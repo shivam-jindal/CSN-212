4f52ca4549f76a55b2a0c481cc0a21653062afed
shivam <shivamjindal39@gmail.com>
2017-02-27 22:36:48 +0530

    added solution of problem 2

--- Assignment-2(DP)/problem2.cpp
+++ Assignment-2(DP)/problem2.cpp
@@ -1,21 +1,44 @@
 #include <bits/stdc++.h>
+#include <vector>
+#include <string.h>
 using namespace std;
 
-long numWays(int width, int height, string[] bad){
-    long answer = 0;
+/*function to check whether the move is blocked or not*/
+bool ifMoveBlocked(vector<string> bad, int a, int b, int c, int d){
+    //convert in form of elements of bad array
+    string move1 = to_string(a)+" "+to_string(b)+" "+to_string(c)+" "+to_string(d);
+    string move2 = to_string(c)+" "+to_string(d)+" "+to_string(a)+" "+to_string(b);
 
+    bool move1Found = find(bad.begin(), bad.end(), move1) != bad.end();
+    bool move2Found = find(bad.begin(), bad.end(), move2) != bad.end();
 
-    return answer;
+    return move1Found || move2Found;
+    }
+
+/*function to return all possible number of distinct paths*/
+long numWays(int width, int height, vector<string> bad){
+    long dp[height+1][width+1];
+	memset(dp, 0, sizeof(dp));
+	dp[0][0] = 1;
+
+	for(int i=0; i<=height; i++){
+		for(int j=0; j<=width; j++){
+			if ((!ifMoveBlocked(bad, j,i-1,j,i)) && (i!=0))
+				dp[i][j] += dp[i-1][j];
+			if ((!ifMoveBlocked(bad, j-1,i,j,i)) && (j!=0))
+				dp[i][j] += dp[i][j-1];
+		}
+	}
+	return dp[height][width];
 }
 
 int main(){
     //check for a test input
     int width = 6;
     int height = 6;
-    string[] bad = {"0 0 0 1","6 6 5 6"};
+    vector<string> bad = {"0 0 0 1","6 6 5 6"};
 
-    cout<<"Number of distinct paths for test input is "<<numWays(width, height, bad)<<endl;
+    cout<<"Number of distinct paths for test input is "<< numWays(width, height, bad)<<endl;
 
     return 0;
 }
-
